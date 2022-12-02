116eed424890f289d12909f8aff552cb1e6ae3ac
Irina <tes7@rambler.ru>
2022-12-02 21:29:43 +0300

    out max number

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа вывода наибольшего из двух чисел
+int numberA = 5;
+int numberB = 7;
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
+// Программа вывода наибольшего из двух чисел
+int numberA = 5;
+int numberB = 7;
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
