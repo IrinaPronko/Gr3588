918a82063871dc72794002bb1b695f4ca268ad9c
Irina <tes7@rambler.ru>
2022-12-03 20:25:07 +0300

    out max two numbers, three numbers, ignore

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -4,15 +4,18 @@ System.Console.Write("первое число - ");
 string? num1line = System.Console.ReadLine();
 System.Console.Write("второе число - ");
 string? num2line = System.Console.ReadLine();
-int numberA = int.Parse(num1line);
-int numberB = int.Parse(num2line);
-System.Console.Write("Наибольшее введенное число - ");
-if(numberA > numberB)
+if(num1line!=null&&num2line!=null)
 {
-System.Console.WriteLine(numberA);
-}
-else
-{
-System.Console.WriteLine(numberB);
+    int numberA = int.Parse(num1line);
+    int numberB = int.Parse(num2line);
+    System.Console.Write("Наибольшее введенное число - ");
+    if(numberA > numberB)
+    {
+    System.Console.WriteLine(numberA);
+    }
+    else
+    {
+    System.Console.WriteLine(numberB);
+    }
 }
 
---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Принимает 3 числа и выдает максимальное
+System.Console.WriteLine("Введите три числа!");
+System.Console.Write("первое число - ");
+string? num1line = System.Console.ReadLine();
+System.Console.Write("второе число - ");
+string? num2line = System.Console.ReadLine();
+System.Console.Write("третье число - ");
+string? num3line = System.Console.ReadLine(); 
+if(num1line!=null&&num2line!=null&&num3line!=null)
+{
+    int numberA = int.Parse(num1line);
+    int numberB = int.Parse(num2line);
+    int numberC = int.Parse(num3line);
+    int maxnumber = numberA; 
+
+    System.Console.Write("Наибольшее введенное число - ");
+    if(maxnumber < numberB)
+    {
+    maxnumber = numberB;
+    }
+    if(maxnumber < numberC)
+    {
+    maxnumber = numberC;  
+    }
+System.Console.WriteLine(maxnumber);
+}
+
