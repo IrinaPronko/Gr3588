8a213aba361a3d21032426662e2ab2811f1fdc32
Irina <tes7@rambler.ru>
2022-12-03 20:50:43 +0300

    get even number

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимает число и выдает четное или нет
+System.Console.Write("Введите число - ");
+string? number1line = System.Console.ReadLine();
+if(number1line!=null)
+{
+    int numberA = int.Parse(number1line);
+    if(numberA % 2==1)
+    {
+    System.Console.WriteLine("введено нечетное число");
+    }
+    else    
+    {
+    System.Console.WriteLine("введено четное число");    
+    }
+}
+
