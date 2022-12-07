1285b1181fb0797ebdb726317fde1fa871ee0631
Irina <tes7@rambler.ru>
2022-12-07 21:04:25 +0300

    out third number or is not third number

---
+++ Sem2Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+System.Console.Write("Введите число - ");
+string? number1line = System.Console.ReadLine();
+if(number1line!=null)
+{
+    if(number1line.Length > 2)
+        System.Console.WriteLine("третья цифра этого числа -> " + number1line[2]);
+    else
+        System.Console.WriteLine("третьей цифры нет");
+}
+
