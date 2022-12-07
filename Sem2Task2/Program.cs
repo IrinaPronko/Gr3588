9f6db45e672fad994c4b90e5bfe7f27866db4c46
Irina <tes7@rambler.ru>
2022-12-07 22:06:00 +0300

    day of week do not dayout

---
+++ Sem2Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+System.Console.Write("Введите день недели - ");
+string? number1line = System.Console.ReadLine();
+if(number1line!=null)
+{
+    int numberDayWeek = int.Parse(number1line);
+    if (numberDayWeek == 6 || numberDayWeek == 7)
+        System.Console.WriteLine("Этот день недели является выходным");
+    else
+    {
+        if (numberDayWeek < 1 || numberDayWeek > 7)
+            System.Console.WriteLine("Вы ввели неправильный номер дня недели");
+        else
+            System.Console.WriteLine("Этот день недели не является выходным");
+    }
+}
