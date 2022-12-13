332b8d91cc73fad390b5e1f641453efa6149be28
Irina <tes7@rambler.ru>
2022-12-13 20:56:33 +0300

    out sum of numbers

---
+++ Sem4Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+// метод ввода значений
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод вывода на экран
+void PrintData(string msg, int res)
+{
+    Console.WriteLine(msg, res);
+}
+
+// метод суммирования цифр числа
+int SumDigit(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+    return res;
+    
+}
+// основная часть программы
+int num = ReadData("Введите число --->  ");
+int res = SumDigit(num);
+PrintData ("Сумма цифр введенного числа равна ---> {0}", res);
