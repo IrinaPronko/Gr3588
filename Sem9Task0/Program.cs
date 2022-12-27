b95df3d762d0715c673cfd6381e256534e4e2a46
Irina <tes7@rambler.ru>
2022-12-27 15:00:10 +0300

    out numbers from N to 1

---
+++ Sem9Task0/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа принимает число N и выводит все натуральные числа в промежутке от N до 1
+// программа
+int num = ReadData("Введите число --->  ");
+string resultLine = LineGenRec(num);
+PrintDataStr(resultLine);
+
+//метод ввода данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод вывода на экран строки результата
+void PrintDataStr(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// рекурсивный метод формирования строки натуральных чисел
+string LineGenRec(int num)
+{
+    if (num <= 0)
+    {
+        return String.Empty;
+    }
+    else
+    {
+        string outLine = Convert.ToString(num) + " " + LineGenRec(num - 1);
+        return outLine;
+    }
+}
